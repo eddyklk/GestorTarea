39ee2acad47342c8387c441dff4c7b67dfe5b1f2
Eddy Junior Valerio Gerardino <eddy.valerio@nordestana.com>
2024-07-13 09:41:50 -0400

    Organizando las clases.

---
+++ GestorTarea/Tarea.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorTarea
+{
+    public class Tarea
+    {
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public int Prioridad { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public string Categoria { get; set; }
+
+        public Tarea(string titulo, string descripcion, int prioridad, DateTime fechaVencimiento, string categoria)
+        {
+            Titulo = titulo;
+            Descripcion = descripcion;
+            Prioridad = prioridad;
+            FechaVencimiento = fechaVencimiento;
+            Categoria = categoria;
+        }
+
+        public override string ToString()
+        {
+            return $"{Titulo}: {Descripcion} (Prioridad: {Prioridad}, Vence: {FechaVencimiento.ToShortDateString()}, Categoría: {Categoria})";
+        }
+    }
+}
